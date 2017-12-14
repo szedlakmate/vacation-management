e2f984c3d3ac5d8a02ed964834f94ee4c2408468
Mate Szedlak <mateszedlak@invenshure.com>
2017-12-14 14:56:09 +0100

    Ensure DB connection by merging a working solution

--- app/app.py
+++ app/app.py
@@ -1,5 +1,15 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+import simplejson as json
+
+from sqlalchemy.exc import IntegrityError
+
+# DB model functions and classes
+from model import CreateDB, SetUpDB     # Functions
+from model import User                 # Classes
+from model import app as application
+from model import db
+
 
 app = Flask(__name__)
 
@@ -8,8 +18,33 @@ def index():
     return 'Hello World!'
 
 
+def createUserTable():
+    try:
+        db.create_all()
+        return json.dumps({'status': True})
+    except IntegrityError:
+        db.session.rollback()
+        return json.dumps({'status': False})
+
+
+@app.route('/reset')
+def createDatabase():
+    HOSTNAME = 'mysqlserver'
+    try:
+        HOSTNAME = request.args['hostname']
+    except:
+        pass
+    database = CreateDB(hostname='mysqlserver')
+    createUserTable()
+    return redirect(url_for('setup'))
+
+
+@app.route('/setup')
+def setup():
+    SetUpDB()
+    return redirect(url_for('index'))
+
+
 if __name__ == "__main__":
     ssl_context=('./app/self.vacation.crt','./app/self.vacation.key')
     app.run(host="0.0.0.0", port=5000, debug=True, ssl_context=ssl_context)
-    #app.add_url_rule('/favicon.ico',
-    #                 redirect_to=url_for('static', filename='img/favicon.ico'))
--- app/model.py
+++ app/model.py
@@ -14,6 +14,9 @@ PASSWORD = 'password'
 USER = 'root'
 HOSTNAME = 'mysqlserver'
 
+BASEUSERS = [] #[{'id':1, 'username':'root', 'nickname':'root', 'google_id':'0', 'avatar':None, 'email': 'foo:bar', 'birthday':'1900-01-01', 'account_status':1}]
+BASECALENDARS = [{'id':1, 'name':'Normal holiday'},{'id':2, 'name':'Sick-leave'}]
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s/%s'%(USER, PASSWORD, HOSTNAME, DATABASE)
 db = SQLAlchemy(app)
@@ -49,6 +52,37 @@ class CreateDB():
         engine = sqlalchemy.create_engine('mysql://%s:%s@%s'%(USER, PASSWORD, HOSTNAME)) # connect to server
         ResetDB(engine)
         engine.execute("CREATE DATABASE IF NOT EXISTS %s "%(DATABASE)) #create db
+
+
+class SetUpDB():
+    def __init__(self):
+        #import sqlalchemy
+        from sqlalchemy.exc import IntegrityError
+        import simplejson as json
+        for user in BASEUSERS:
+            try:
+                profile = User(username=user['username'], nickname=user['nickname'], google_id=user['google_id'],
+                               avatar=user['avatar'], email=user['email'], birthday=user['birthday'], account_status=user['account_status'], id=user['id'])
+                db.session.add(profile)
+                db.session.commit()
+            #except KeyError:  # IntegrityError:
+            #    db.session.rollback()
+            except IntegrityError:
+                db.session.rollback()
+                return json.dumps(
+                    {'Integrity error was raised:': 'Please check the given data or contact the administrator'})
+        """for calendar in BASECALENDARS:
+            try:
+                newcalendar = CalendarType(name=calendar['name'], id=calendar['id'])
+                db.session.add(newcalendar)
+                db.session.commit()
+            except KeyError:  # IntegrityError:
+                db.session.rollback()
+            except IntegrityError:
+                db.session.rollback()
+                return json.dumps(
+                    {'Integrity error was raised:': 'Please check the given data or contact the administrator'})
+"""
 
 
 def ResetDB(engine):
